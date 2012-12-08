fb240089c018d09581b6aabda7175c63ce91bb04
virgil (Virgilio Gómez Rubio) <virgilio.gomez@uclm.es>
2012-12-08 18:14:10 +0000

    Minor changes to Boston example.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdep2@70 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/test_sar.R
+++ pkg/R/test_sar.R
@@ -15,16 +15,24 @@ prior = NULL
 
 #prior$metflag<-0#M-H sampler
 
-y<-matrix(rnorm(3), ncol=1)
-y<-solve(diag(3)-0.9*W)%*%y
-x<-matrix(rnorm(6),ncol=2)
+nsim<-3
+y<-matrix(rnorm(nsim), ncol=1)
+x<-matrix(rnorm(2*nsim),ncol=2)
+y<-solve(diag(nsim)-0.9*W, y+x%*%matrix(c(0,5), ncol=1))
 
-#prior$novi_flag<-0
+
 ndraw=1000
-nomit=0
+nomit=500
+
 source('sar_g.R')
+
+prior<-prior_parse(NULL, k=2)
+prior$rmin<-.01
+prior$rmax<-.99
+prior$novi_flag<-1
+
 results=sar_g(y,x,W,ndraw,nomit,prior)
-plot(results$pdraw)
+plot(density(results$pdraw), type="l", main="rho")
 
 
 
@@ -45,16 +53,49 @@ y<-solve(diag(nrow(W))-0.4*W)%*%y
 #Heteroc.  model
 prior<-prior_parse(NULL, k=2)
 prior$novi_flag<-0
-results<-sar_g(y, x, W, 150, 50, prior)
-plot(results$pdraw, type="l", main="rho")
+results<-sar_g(y, x, W, 1500, 500, prior)
+plot(density(results$pdraw), type="l", main="rho")
 summary(results$pdraw)
 
 
 
 #Homoc. model
 prior$novi_flag<-1
-results1<-sar_g(y, x, W, 150, 50, prior)
-plot(results1$pdraw, type="l", main="rho")
+results1<-sar_g(y, x, W, 1500, 500, prior)
+plot(density(results1$pdraw), type="l", main="rho")
 summary(results1$pdraw)
+
+
+
+
+#Example with Boston housing data
+#Code from help(boston, package="spdep")
+hr0 <- lm(log(MEDV) ~ CRIM + ZN + INDUS + CHAS + I(NOX^2) + I(RM^2) +
+      AGE + log(DIS) + log(RAD) + TAX + PTRATIO + B + log(LSTAT)
+, data=boston.c)
+
+y<-matrix(log(boston.c$MEDV), ncol=1)
+x<-model.matrix(hr0)
+xlag<-cbind(x, W%*%x[,-1])
+
+prior<-prior_parse(NULL, k=ncol(x))
+prior$novi_flag<-0
+prior$metflag<-1
+
+resultsx<-sar_g(y, x, W, 3000, 1000, prior)
+#plot(results$pdraw, type="l", main="rho")
+plot(density(resultsx$pdraw), type="l", main="rho")
+summary(resultsx$pdraw)
+cbind(apply(resultsx$bdraw, 2, mean), apply(resultsx$bdraw, 2, sd))
+
+#With lagged covariates
+prior<-prior_parse(NULL, k=ncol(xlag))
+prior$novi_flag<-0
+prior$metflag<-1
+resultsxlag<-sar_g(y, xlag, W, 3000, 1000, prior)
+#plot(results$pdraw, type="l", main="rho")
+plot(density(resultsxlag$pdraw), type="l", main="rho")
+summary(resultsxlag$pdraw)
+cbind(apply(resultsxlag$bdraw, 2, mean), apply(resultsxlag$bdraw, 2, sd))
 
 
