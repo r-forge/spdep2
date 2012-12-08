2ad9b301a2057b19c884a90dbe6ff158fc8aaf8e
virgil (Virgilio Gómez Rubio) <virgilio.gomez@uclm.es>
2012-12-08 17:49:19 +0000

    Checked and fixed.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdep2@69 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/sar_g.R
+++ pkg/R/sar_g.R
@@ -1,84 +1,84 @@
-sar_parse <- function(prior,k){
-#% set defaults
-
-eflag = 0;    # % default to not computing eigenvalues
-ldetflag = 1;  #% default to 1999 Pace and Barry MC determinant approx
-mflag = 1;     #% default to compute log marginal likelihood
-order = 50;    #% there are parameters used by the MC det approx
-iter = 30;     #% defaults based on Pace and Barry recommendation
-rmin = -1;     #% use -1,1 rho interval as default
-rmax = 1;
-detval = 0;    #% just a flag
-rho = 0.5;
-sige = 1.0;
-rval = 4;
-mm = 0;
-kk = 0;
-nu = 0;
-d0 = 0;
-a1 = 1.01;
-a2 = 1.01;
-c_beta = matrix(rep(0,k),k,1); #defuse prior for beta
-Tbeta = diag(k)*1e+12; ## Abhirup: ??
-prior_beta = 0;   #% flag for diffuse prior on beta
-novi_flag = 0; #% do vi-estimates
-inform_flag = 0;
-
-# check with user input
-if(length(prior$novi)!=0) novi_flag=prior$novi
-if(length(prior$nu)!=0) nu=prior$nu
-if(length(prior$d0)!=0) d0=prior$d0
-if(length(prior$rval)!=0) rval=prior$rval
-if(length(prior$a1)!=0) a1=prior$a1
-if(length(prior$a2)!=0) a2=prior$a2
-if(length(prior$m)!=0){ 
-	mm=prior$m
-	kk=prior$k
-	rval=rgamma(1,mm,1/kk)
-	}
-if(length(prior$m)!=0){ 
-	c=prior$beta
-	inform_flag=1
-	}
-if(length(prior$rmin)!=0) {
-	rmin=prior$rmin
-	eflag=0
-	}
-if(length(prior$rmax)!=0) {
-	rmin=prior$rmax
-	eflag=0
-	}
-if(length(prior$lndet)!=0){
-	detval = prior$lndet;
-	ldetflag = -1;
-	eflag = 0;
-	rmin = detval; ##detval[1,1]
-	nr = length(detval);
-	rmax = detval[nr]; ###detval[nr,1]
-	}
-if(length(prior$lflag)!=0){
-	tst = prior$lflag;
-        if (tst == 0)        ldetflag = 0; 
-        if (tst == 1)        ldetflag = 1; 
-        if (tst == 2)        ldetflag = 2;
-	}
-if(length(prior$order)!=0) order=prior$order
-if(length(prior$iter)!=0) iter=prior$iter
-if(length(prior$eig)!=0) eflag=prior$eig
-return (c(nu,d0,rval,mm,kk,rho,sige,rmin,rmax,detval,ldetflag,eflag,order,iter,novi_flag,c,Tbeta,inform_flag,a1,a2))
-}
+#sar_parse <- function(prior,k){
+##% set defaults
+#
+#eflag = 0;    # % default to not computing eigenvalues
+#ldetflag = 1;  #% default to 1999 Pace and Barry MC determinant approx
+#mflag = 1;     #% default to compute log marginal likelihood
+#order = 50;    #% there are parameters used by the MC det approx
+#iter = 30;     #% defaults based on Pace and Barry recommendation
+#rmin = -1;     #% use -1,1 rho interval as default
+#rmax = 1;
+#detval = 0;    #% just a flag
+#rho = 0.5;
+#sige = 1.0;
+#rval = 4;
+#mm = 0;
+#kk = 0;
+#nu = 0;
+#d0 = 0;
+#a1 = 1.01;
+#a2 = 1.01;
+#c_beta = matrix(rep(0,k),k,1); #defuse prior for beta
+#Tbeta = diag(k)*1e+12; ## Abhirup: ??
+#prior_beta = 0;   #% flag for diffuse prior on beta
+#novi_flag = 0; #% do vi-estimates
+#inform_flag = 0;
+#
+## check with user input
+#if(length(prior$novi)!=0) novi_flag=prior$novi
+#if(length(prior$nu)!=0) nu=prior$nu
+#if(length(prior$d0)!=0) d0=prior$d0
+#if(length(prior$rval)!=0) rval=prior$rval
+#if(length(prior$a1)!=0) a1=prior$a1
+#if(length(prior$a2)!=0) a2=prior$a2
+#if(length(prior$m)!=0){ 
+#	mm=prior$m
+#	kk=prior$k
+#	rval=rgamma(1,mm,1/kk)
+#	}
+#if(length(prior$m)!=0){ 
+#	c=prior$beta
+#	inform_flag=1
+#	}
+#if(length(prior$rmin)!=0) {
+#	rmin=prior$rmin
+#	eflag=0
+#	}
+#if(length(prior$rmax)!=0) {
+#	rmin=prior$rmax
+#	eflag=0
+#	}
+#if(length(prior$lndet)!=0){
+#	detval = prior$lndet;
+#	ldetflag = -1;
+#	eflag = 0;
+#	rmin = detval; ##detval[1,1]
+#	nr = length(detval);
+#	rmax = detval[nr]; ###detval[nr,1]
+#	}
+#if(length(prior$lflag)!=0){
+#	tst = prior$lflag;
+#        if (tst == 0)        ldetflag = 0; 
+#        if (tst == 1)        ldetflag = 1; 
+#        if (tst == 2)        ldetflag = 2;
+#	}
+#if(length(prior$order)!=0) order=prior$order
+#if(length(prior$iter)!=0) iter=prior$iter
+#if(length(prior$eig)!=0) eflag=prior$eig
+#return (c(nu,d0,rval,mm,kk,rho,sige,rmin,rmax,detval,ldetflag,eflag,order,iter,novi_flag,c,Tbeta,inform_flag,a1,a2))
+#}
 
 #### start sar_g #####
 sar_g <- function(y,x,W,ndraw,nomit,prior){
 
 
 #% error checking on inputs
-n=dim(y)[1]
-junk = dim(y)[2]
-n1=dim(x)[1]
-k=dim(x)[2]
-n2=dim(W)[1]
-n4=dim(W)[2]
+n=nrow(y)
+junk = ncol(y)
+n1=nrow(x)
+k=ncol(x)
+n2=nrow(W)
+n4=ncol(W)
 
 
 #time1 = 0
@@ -98,8 +98,8 @@ results$y = y;
 #eflag,order,iter,novi_flag,c,Tbeta,inform_flag,a1,a2] = sar_parse(prior,k);
     
 #temp=sar_parse(prior,k)
-temp=prior_parse(prior,k)
-attach(temp)
+pprior=prior_parse(prior,k)
+attach(pprior)
 #nu=temp[1]
 #d0=temp[2]
 #rval=temp[3]
@@ -122,37 +122,55 @@ attach(temp)
 #a2=temp[20]
 
 # check if the user handled the intercept term okay
-#    n = ncol(y)
-#    if (sum(x[,1]) != n){
-#    tst = sum(x); % we may have no intercept term
-#    ind = find(tst == n); % we do have an intercept term
-#     if length(ind) > 0
-#     error('sar_g: intercept term must be in first column of the x-matrix');
-#     elseif length(ind) == 0 % case of no intercept term
-#     cflag = 0;
-#     p = size(x,2);
-#     end;
-#    elseif sum(x(:,1)) == n % we have an intercept in the right place
-#     cflag = 1;
-#     p = size(x,2)-1;
-#    end;
-#     
-#    results.cflag = cflag;
-#    results.p = p;
-    
-#    if n1 ~= n2
-#    error('sar_g: wrong size weight matrix W');
-#    elseif n1 ~= n
-#    error('sar_g: wrong size weight matrix W');
-#    end;
-#    [nchk junk] = size(y);
-#    if nchk ~= n
-#    error('sar_g: wrong size y vector input');
-#    end;
-#   
+#FIXME: We may change this later using the formula, for example
+n = nrow(y)
+if (sum(x[,1]) != n)
+{
+	tst = apply(x, 2, sum);#we may have no intercept term
 
-#results.order = order;
-#results.iter = iter;
+	ind = which(tst == n);#we do have an intercept term
+	if(length(ind) > 0)
+	{
+		stop('sar_g: intercept term must be in first column of the x-matrix');
+	}
+	else
+	{
+		if(length(ind) == 0)# case of no intercept term
+		{
+			cflag = 0;
+			p = ncol(x);
+		}
+	}
+}
+else
+{
+	if(sum(x[,1]) == n)# % we have an intercept in the right place
+	{
+		cflag = 1;
+		p = ncol(x)-1;
+	}
+}
+     
+results$cflag = cflag;
+results$p = p;
+    
+if(n1!=n2)
+{
+	stop('sar_g: wrong size weight matrix W');
+}
+else
+{
+	if(n1!=n)
+		stop('sar_g: wrong size weight matrix W');
+}
+
+#    [nchk junk] = size(y);
+nchk <- nrow(y)
+if(nchk!=n)
+	stop('sar_g: wrong size y vector input');
+
+results$order = order;
+results$iter = iter;
 
 #timet = clock; % start the timer
 
@@ -172,11 +190,13 @@ results$iter = iter;
 
 
 #% storage for draws
-bsave = matrix(rep(0,(ndraw-nomit)*k),ndraw-nomit,k)
-if (mm !=0) rsave = matrix(rep(0,ndraw-nomit),ndraw-nomit,1)
-psave = matrix(rep(0,ndraw-nomit),ndraw-nomit,1)
-ssave = matrix(rep(0,ndraw-nomit),ndraw-nomit,1)
-vmean= matrix(rep(0,n),n,1)
+bsave = matrix(0,ndraw-nomit,k)
+if (mm !=0) 
+	rsave = matrix(0,ndraw-nomit,1)
+
+psave = matrix(0, ndraw-nomit,1)
+ssave = matrix(0, ndraw-nomit,1)
+vmean= matrix(0, n,1)
 
 
 #% ====== initializations
@@ -199,38 +219,39 @@ if (novi_flag==0){
 #hwait = waitbar(0,'sar: MCMC sampling ...');
 
 #t0 = clock;                  
-iter = 1;
+	iter = 1;
           while (iter <= ndraw){ #% start sampling;
                   
           #% update beta   
           xs = matmul(x,sqrt(V)); ## code for matmul in support functions
           ys = sqrt(V)*y;
           Wys = sqrt(V)*Wy;
-          AI = solve((t(xs)%*%xs + sige*TI),diag(rep(1,k)));         
+          AI = solve((t(xs)%*%xs + sige*TI))#,diag(rep(1,k)));         
           yss = ys - rho*Wys;          
           xpy = t(xs)%*%yss;
           b = t(xs)%*%yss + sige*TIc;
           b0 = solve((t(xs)%*%xs + sige*TI),b);
-          bhat = norm_rnd(sige*AI) + b0; ##code for norm_rnd ?? check  
+          #bhat = norm_rnd(sige*AI) + b0; ##code for norm_rnd ?? check  
+          bhat = mvrnorm(1, b0, sige*AI)##From MASS package
           xb = xs%*%bhat; 
                     
           #% update sige
           nu1 = n + 2*nu; 
           e = (yss - xb);
           d1 = 2*d0 + t(e)%*%e;
-          chi = chis_rnd(1,nu1); ##code for chis_rnd
+          chi = rchisq(1,nu1);
           sige = as.numeric(d1/chi); 
 
 	  #% update vi
           ev = y - rho*Wy - x%*%bhat; 
           #chiv = chis_rnd(n,rval+1);  
-          chiv = matrix(rchisq(n,rval+1),n,1); #% Statistics Toolbox function ##why not use this all the time ??
+          chiv = matrix(rchisq(n,rval+1),n,1);
           vi = (ev*ev/sige + In%*%rval)/chiv;
           V = In/vi; 
                         
           #% update rval
-          if (mm != 0)   rval = dgamma(1, shape=mm, rate=kk)#gamm_rnd(1,1,mm,kk);  
-          
+          if (mm != 0)   
+		rval = dgamma(1, shape=mm, rate=kk)#gamm_rnd(1,1,mm,kk);  
           
       #% we use griddy Gibbs to perform rho-draw
 #          b0 = (t(xs)%*%xs + sige%*%TI )\(t(xs)%*%ys + sige%*%TIc);
@@ -250,12 +271,13 @@ iter = 1;
         ssave[iter-nomit,1] = sige;
         psave[iter-nomit,1] = rho;
         vmean = vmean + vi; 
-    	if (mm != 0)   rsave[iter-nomit,1] = rval
+    	if (mm != 0)
+		rsave[iter-nomit,1] = rval
     	         
     }
 
-iter = iter + 1; 
-}
+	iter = iter + 1; 
+	}
                     
 ##waitbar(iter/ndraw);         
 }   ##% end of sampling loop
@@ -281,11 +303,11 @@ xpWy = t(x)%*%Wy;
           while (iter <= ndraw){ #% start sampling;
                   
           ##% update beta   
-          AI = solve((xpx + sige*TI),diag(rep(1,k)));        
+          AI = solve((xpx + sige*TI))#,diag(rep(1,k)));        
           ys = y - rho*Wy;          
           b = t(x)%*%ys + sige*TIc;
           b0 = solve((xpx + sige*TI),b);
-          bhat = norm_rnd(sige*AI) + b0;  
+          bhat = mvrnorm(1, b0, Sigma); #norm_rnd(sige*AI) + b0;  
           xb = x%*%bhat;
           
           ##% update sige
@@ -297,7 +319,7 @@ xpWy = t(x)%*%Wy;
           sige = as.numeric(d1/chi);
           
           ###% update rho using griddy Gibbs
-          AI = solve((xpx + sige*TI),diag(rep(1,k)));
+          AI = solve((xpx + sige*TI))#,diag(rep(1,k)));
           b0 = solve((xpx + sige*TI),(xpy + sige*TIc));
           bd = solve((xpx + sige*TI),(xpWy + sige*TIc));
           e0 = y - x%*%b0;
@@ -314,9 +336,9 @@ xpWy = t(x)%*%Wy;
     vmean = vmean + vi; 
     }
                     
-iter = iter + 1; 
-##waitbar(iter/ndraw);         
-}### % end of sampling loop
+   iter = iter + 1; 
+   ##waitbar(iter/ndraw);         
+   }### % end of sampling loop
 ##close(hwait);
 
 ##time3 = etime(clock,t0);
@@ -338,8 +360,9 @@ uiter=50;
 maxorderu=100;
 nobs = n;
 rv=matrix(rnorm(nobs*uiter),nobs,uiter);
-tracew=matrix(rep(0,maxorderu),maxorderu,1);
+tracew=matrix(0, maxorderu,1);
 wjjju=rv;
+
 for (jjj in 1:maxorderu){
     wjjju=W%*%wjjju;
 #    tracew[jjj]=mean(mean(rv*wjjju));
@@ -348,14 +371,14 @@ for (jjj in 1:maxorderu){
 }
 
 traces=tracew;
-traces[1]=0;
-traces[2]=sum(t(W)%*%W)/nobs;
-trs=matrix(c(1,traces));
+traces[1,1]=0;
+traces[2,1]=sum(t(W)*W)/nobs;
+trs=matrix(c(1,traces), ncol=1);
 ntrs=length(trs);
 trbig=t(trs);
                  
         if (cflag == 1){
-        bdraws = bsave[,2:length(bsave)];
+        bdraws = bsave[,-1];#2:length(bsave)];
         }
 	if(cflag == 0){
         bdraws = bsave;
@@ -364,20 +387,17 @@ trbig=t(trs);
 
         ree = 0:(ntrs-1);
 
-#Number of covariates minus intercept
-p<-(k-1)#FIXME: Check whether intercept is used or not 
-
-        rmat = matrix(rep(0,ntrs),1,ntrs);####three dimentional matrix in R??
+        rmat = matrix(0, 1,ntrs);####three dimentional matrix in R??
 #        total = matrix(rep(0,(ndraw-nomit)*p*ntrs)),ndraw-nomit,p,ntrs);
-        total = array(rep(0,(ndraw-nomit)*p*ntrs),dim=c(ndraw-nomit,p,ntrs));
+        total = array(0, dim=c(ndraw-nomit,p,ntrs));
 #        direct = zeros(ndraw-nomit,p,ntrs);
-        direct = array(rep(0,(ndraw-nomit)*p*ntrs),dim=c(ndraw-nomit,p,ntrs));
+        direct = array(0,dim=c(ndraw-nomit,p,ntrs));
 #        indirect = zeros(ndraw-nomit,p,ntrs);
-        indirect = array(rep(0,(ndraw-nomit)*p*ntrs),dim=c(ndraw-nomit,p,ntrs));
+        indirect = array(0,dim=c(ndraw-nomit,p,ntrs));
        
 for (i in 1:(ndraw-nomit))
 {
-    rmat = pdraws[i,1]^ree;#VIRGILIO: Check this
+    rmat = pdraws[i,1]^ree;#FIXME: Check this
     for (j in 1:p)
 	{
             bbeta = bdraws[i,j];#VIRGILIO: Check this
@@ -402,8 +422,8 @@ V = In/vmean;
 
 
 results$sige = sige;
-nobs=dim(x)[1]
-nvar=dim(x)[2]
+nobs=nrow(x)
+nvar=ncol(x)
 #[nobs,nvar] = size(x);
           xs = matmul(x,sqrt(V));
           ys = sqrt(V)*y;
@@ -422,7 +442,7 @@ nvar=dim(x)[2]
   if(inform_flag == 1){
    mlike = rho_marginal2(detval,e0,ed,epe0,eped,epe0d,nobs,nvar,logdetx,a1,a2,c,TI,xs,ys,sige,W);
   }
- yhat = solve((diag(rep(1,nobs)) - rho*W),(x%*%beta));
+ yhat = solve((diag(nobs) - rho*W),(x%*%beta));
  e = y - yhat;
 
 ##% compute R-squared
@@ -460,7 +480,7 @@ results$bmean = c;
 results$bstd  = sqrt(diag(Tbeta));
 results$ndraw = ndraw;
 results$nomit = nomit;
-results$time  = time;
+#results$time  = time;
 results$nu = nu;
 results$d0 = d0;
 results$a1 = a1;
@@ -485,6 +505,8 @@ results$rdraw = 0;
 
 }
 
+
+detach(pprior)
 return(results)
 }#### end of sar_g
 
