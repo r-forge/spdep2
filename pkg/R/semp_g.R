f0d8cd5ed97c35f8db66b0708c1e7b4c74dc2e13
virgil (Virgilio Gómez Rubio) <virgilio.gomez@uclm.es>
2012-12-18 09:32:08 +0000

    Minor fixes. sige set to 1 and not updated in semp_g() because of identifiability problems.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spdep2@81 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/semp_g.R
+++ pkg/R/semp_g.R
@@ -1,24 +1,78 @@
 semp_g <- function(y,x,W,ndraw,nomit,prior){
 
-n=dim(y)[1]
-junk=dim(y)[2]
+n=nrow(y)
+junk=ncol(y)
 yin=y
 results <-list()
 results$y=y
-n1=dim(x)[1]
-k=dim(x)[2]
-n3=dim(W)[1]
-n4=dim(W)[2]
+n1=nrow(x)
+k=ncol(x)
+n3=nrow(W)
+n4=ncol(W)
 
-temp=prior_parse(prior,k)
-attach(temp)
+
+
+if(n1!=n) 
+{
+	stop('semp_g: x-matrix contains wrong # of observations');
+}
+else
+{
+	if(n3!=n4)
+	{
+		stop('semp_g: W matrix is not square');
+	}
+	else
+	{
+		if(n3!=n)
+			stop('semp_g: W matrix is not the same size at y,x');
+	}
+}
+
+
+pprior=prior_parse(prior,k)
+attach(pprior)
 
 results$order=order
 results$iter=iter
 
+# error checking on prior information inputs
+
+checkk=nrow(c_beta)
+junk = ncol(c_beta)
+
+if(checkk!=k)
+{
+	stop('semp_g: prior means are wrong');
+}
+else
+{
+	if(junk!=1)
+		stop('semp_g: prior means are wrong');
+}
+
+checkk=nrow(Tbeta)
+junk = ncol(Tbeta)
+
+if(checkk!=k)
+{
+	stop('semp_g: prior bcov are wrong');
+}
+else
+{
+	if(junk!=k)
+		stop('semp_g: prior bcov are wrong');
+}
+
+
 out_temp = set_eigs(eflag,W,rmin,rmax,n)
 rmin=out_temp$rmin
 rmax=out_temp$rmax
+
+results$rmin=rmin
+results$rmax=rmax
+results$lflag=ldetflag
+
 detval = set_lndet(ldetflag, W, rmin, rmax, detval, order, iter)
 
 bsave=matrix(0,ndraw-nomit,k)
@@ -28,7 +82,7 @@ ymean=matrix(0,n,1)
 yhat=matrix(0,n,1)
 acc_rate=matrix(0,ndraw,1)
 
-TI=solve(T)
+TI=solve(Tbeta)
 TIc=TI%*%c_beta
 iter=1
 In=matrix(1,n,1)
@@ -43,33 +97,42 @@ W2diag=	diag(t(W)%*%W)			##spdiags(t(W)%*%W,0) ##spdiags()
 iter=1
 acc=0
 
+sige=1#This is set by default and not updated because of identif. problems
+
 while(iter <= ndraw){
+
+	#Update beta
 	xs=x-rho*Wx
-	AI=solve(t(xs)%*%xs+as.numeric(sige)*TI)
+	AI=solve(t(xs)%*%xs+sige*TI)
 	ys=y-rho*Wy
-	b=t(xs)%*%ys+as.numeric(sige)*TIc
+	b=t(xs)%*%ys+sige*TIc
 	b0=AI%*%b
-	bhat=norm_rnd(as.numeric(sige)*AI)+b0
+	bhat=mvrnorm(1, b0, sige*AI)
 	
-	nu1=n+2*nu
-	e=ys-xs%*%bhat
-	d1=2*d0+t(e)%*%e
-	chi=chis_rnd(1,nu1)
-	sige=d1/chi
+	#Update sige
+	#nu1=n+2*nu
+	#e=ys-xs%*%bhat
+	#d1=2*d0+t(e)%*%e
+	#chi=rchisq(1, nu1)
+	#sige=as.numeric(d1/chi)
+	#sige<-1
 	
+	#Update z-values
 	mu=x%*%bhat
 	ymu=y-mu
 	dsig=matrix(1,n,1)-rho*rho*W2diag
 	yvar=matrix(1,n,1)/dsig
-	A=(1/as.numeric(sige))*(diag(n)-rho*W)%*%ymu
+	A=(1/sige)*(diag(n)-rho*W)%*%ymu
 	B=t(diag(n)-rho*W)%*%A
 	Cy=ymu-yvar*B
 	ym=mu+Cy
 	
 	ind=which(yin==0)
-	y[ind,1]=normrt_rnd(ym[ind,1],yvar[ind,1],0)
+	#y[ind,1]=normrt_rnd(ym[ind,1],yvar[ind,1],0)
+	y[ind,1]=rtnorm(1, mean=ym[ind,1], sd=sqrt(yvar[ind,1]),-Inf, 0)
 	ind=which(yin==1)
-	y[ind,1]=normlt_rnd(ym[ind,1],yvar[ind,1],0)
+	#y[ind,1]=normlt_rnd(ym[ind,1],yvar[ind,1],0)
+	y[ind,1]=rtnorm(1, mean=ym[ind,1], sd=sqrt(yvar[ind,1]),0, Inf)
 	
 	Wy=W%*%y
 	
@@ -78,24 +141,29 @@ while(iter <= ndraw){
 	accept=0
 	rho2=rho+cc*rnorm(1)
 	while(accept==0){
-		if((rho2>rmin)&(rho2<rmax))	accept=1
-		else	rho2=rho+cc*rnorm(1)
+		if((rho2>rmin)&(rho2<rmax))	
+			accept=1
+		else	
+			rho2=rho+cc*rnorm(1)
 		}
 		
 	rhoy=c_rho_sem(rho2,y,x,bhat,sige,W,detval,matrix(1,n,1),a1,a2)
 	ru=runif(1)
-	if((rhoy-rhox) > exp(1))	p=1
-	else {
+#	if(exp(rhoy-rhox) > 1)	
+#		p=1
+#	else {
 		ratio=exp(rhoy-rhox)
 		p=min(1,ratio)
-		}
+#		}
 	if(ru<p){
 		rho=rho2
 		acc=acc+1
 		}	
 	acc_rate[iter,1]=acc/iter
-	if(acc_rate[iter,1]<0.4)	cc=cc/1.1
-	if(acc_rate[iter,1]>0.6)	cc=cc*1.1
+	if(acc_rate[iter,1]<0.4)
+		cc=cc/1.1
+	if(acc_rate[iter,1]>0.6)
+		cc=cc*1.1
 	
 	if(iter>nomit){
 		bsave[iter-nomit,1:k]=t(bhat)
@@ -111,11 +179,13 @@ bmean=apply(bsave,2,mean)
 #bmean=t(bmean)
 rho=apply(psave,2,mean)
 
-nobs=dim(x)[1]
-nvar=dim(x)[2]
+nobs=nrow(x)
+nvar=ncol(x)
 
-if(inform_flag==0)	mlike=sem_marginal(detval,ymean,x,Wy,Wx,nobs,nvar,a1,a2)
-if(inform_flag !=0) mlike=sem_marginal2(detval,ymean,x,Wy,Wx,nobs,nvar,a1,a2,c,TI,sige)
+if(inform_flag==0)	
+	mlike=sem_marginal(detval,ymean,x,Wy,Wx,nobs,nvar,a1,a2)
+else
+	mlike=sem_marginal2(detval,ymean,x,Wy,Wx,nobs,nvar,a1,a2,c_beta,TI,sige)
 
 yhat=x%*%bmean
 yprob=pnorm(yhat)
@@ -163,33 +233,7 @@ results$novi = novi_flag;
 results$lndet = detval;
 results$priorb = inform_flag;
 
-results
+detach(pprior)
+return(results)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
--- pkg/R/test_semp.R
+++ pkg/R/test_semp.R
@@ -1,18 +1,49 @@
+## test file
+
 library(Matrix)
+library(MASS)
+library(msm)
+library(spdep)
 source('parse.R')
 source('utils.R')
 
-W<-structure(c(0, .5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0), .Dim = c(3L, 
-3L))
+#W<-structure(c(0, .5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0), .Dim = c(3L, 3L))
 
 prior = NULL
+#prior<-prior_parse(NULL,2)
+#prior$ldetflag<-0
+#prior$rmin<-0.1
+#prior$rmax<-0.99#If this is set to 1 then problem when interpolating log-det's
 
-y<-matrix(rnorm(3), ncol=1)
-y<-solve(diag(3)-0.9*W)%*%y
-x<-matrix(rnorm(6),ncol=2)
+#prior$metflag<-0#M-H sampler
 
-ndraw=100
-nomit=20
+nsim<-1000
+xx<-matrix(rnorm(nsim*2), ncol=2)
+W<-nb2mat(knn2nb(knearneigh(xx, 2)))
+
+err<-matrix(1*rnorm(nsim), ncol=1)
+x<-cbind(1, matrix(rnorm(2*nsim),ncol=2))
+#xWx<-cbind(x, W%*%x)
+ystar<-solve(diag(nsim)-0.9*W, err)+x%*%matrix(c(1,5, 10), ncol=1)
+y<-matrix(as.numeric(ystar>0), ncol=1)
+
+
+ndraw=500
+nomit=250
+
 source('semp_g.R')
+
+prior<-prior_parse(NULL, k=3)
+prior$rmin<-.01
+prior$rmax<-.99
+prior$novi_flag<-1
+
 results=semp_g(y,x,W,ndraw,nomit,prior)
-plot(results$pdraw)
+plot(density(results$pdraw), type="l", main="rho")
+
+
+#Fit model to latent variable
+source('sem_g.R')
+results2=sem_g(ystar,x,W,ndraw,nomit,prior)
+
+
